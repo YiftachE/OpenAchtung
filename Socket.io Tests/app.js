542f794529ea50802ee76cda58456f2e4c9ef85a
Yiftach Edelstain <yiftach.work@gmail.com>
2015-02-26 22:04:58 +0200

    Update GitIgnore and add package.json

---
+++ Socket.io Tests/app.js
@@ -0,0 +1,10 @@
+(function() {
+    var http = require('http');
+    http.createServer(function(req, res) {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.end("hello world");
+    }).listen(8080);
+}());
+
