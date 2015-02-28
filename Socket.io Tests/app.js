4e21cbe2b614ec0c121cfbe3592e2b42c17bd087
chenka10 <chenka09@gmail.com>
2015-02-28 16:45:15 +0200

    added stuff

--- Socket.io Tests/app.js
+++ Socket.io Tests/app.js
@@ -1,5 +1,11 @@
 (function() {
     var http = require('http');
+    var io = require('socket.io')(http);
+
+    io.on('connection', function(socket){
+        console.log('a user connected');
+    });
+
     http.createServer(function(req, res) {
         res.writeHead(200, {
             'Content-Type': 'text/plain'
